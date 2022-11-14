048f8fc417a6562ab48a6e36b8208d3e21e17911
khashayar shayesteh <shayesteh.office@gmail.com>
2022-11-14 10:16:26 -0800

    security

--- src/main/java/com/cydeo/TicketingProjectSecurityPackageApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurityPackageApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication  //this includes @Configuration
 public class TicketingProjectSecurityPackageApplication {
@@ -14,8 +16,13 @@ public class TicketingProjectSecurityPackageApplication {
     }
 
     @Bean
-    public ModelMapper mapper(){
+    public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
     }
 
 }
